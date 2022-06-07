34e88b05532027469fe804419938d5a9e05c2303
ABHIJITH U <abhijith.u@edstem.com>
2022-06-07 16:13:15 +0530

    added GET api

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from flask_mongoengine import MongoEngine
 from models import User
 
@@ -12,12 +12,38 @@ db = MongoEngine(app)
 
 @app.route('/data', methods=['POST'])
 def data():
+    if (
+            not request.json
+            or not request.json.get("name")
+
+    ):
+        abort(400)
+
     body = request.json
     try:
         user = User(**body).save()
         return jsonify(user)
     except:
         return "you have an error"
+
+
+@app.route('/data', methods=['GET'])
+def data_display():
+    try:
+        users = User.objects.all()
+        return jsonify(users)
+    except:
+        abort(404)
+
+
+@app.route('/data/<user_id>', methods=['GET'])
+def info_of_user(user_id):
+    try:
+        person = User.objects(id=user_id).get()
+        return jsonify(person)
+    except:
+        abort(404)
+
 
 
 if __name__ == '__main__':
